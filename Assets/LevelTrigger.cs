b8a88d86c60148145b07d6c450261d86e2d32656
DESKTOP-DA3D7HN\rende <renderbydavid@yahoo.com>
2017-12-18 14:18:01 -0600

    Level selection

---
+++ Assets/LevelTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTrigger : MonoBehaviour {
+
+	void OnTriggerEnter (Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Application.LoadLevel("selectlevel");
+        }
+    }
+}
