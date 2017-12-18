b7a3b9e37c230870535754deeceecda7a2ce9822
Boris Pettersson <br.cupofteagames@gmail.com>
2017-12-18 03:16:31 -0400

    Physics Impriovement

---
+++ Assets/Scripts/Broken Dream Scripts/TagBreakCollision.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TagBreakCollision : MonoBehaviour {
+
+    public string[] tagNames;
+    public DestroyableObject destroyableObj;
+
+    void OnTriggerEnter(Collider col)
+    {
+        for(int i = 0;i<tagNames.Length;i++)
+    if(col.gameObject.tag == tagNames[i])
+        {
+            destroyableObj.die();
+        }
+    }
+}
---
+++ Assets/Scripts/Game Scripts/WheelGripMonitor.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CarController))]
+public class WheelGripMonitor : MonoBehaviour
+{
+
+    
+    public Surface[] surfaces;
+    CarController carCon;
+    public WheelCollider[] wheels;
+
+    float sideWaysGrip;
+    float frontGrip;
+
+    WheelFrictionCurve wfc = new WheelFrictionCurve();
+
+    void Awake()
+    {
+        carCon = GetComponent<CarController>();
+    //    for (int i = 0; i < wheels.Length; i++)
+        {
+      //      wheels[i] = carCon.car_Infos[i].leftWheel;
+      //      wheels[i + 1] = carCon.car_Infos[i].rightWheel;
+      //      i++;
+        }
+
+        //Get grip levels
+        for (int i = 0; i < wheels.Length; i++)
+        {
+           sideWaysGrip = wheels[i].sidewaysFriction.stiffness;
+           frontGrip = wheels[i].forwardFriction.stiffness;
+        }
+    }
+
+
+    void Update()
+    {
+        for (int i = 0; i < wheels.Length; i++)
+        {
+            WheelHit hit;
+            if (wheels[i].GetGroundHit(out hit))
+            {
+                if (hasSurface(hit.collider.gameObject.tag))
+                {
+                    wfc = wheels[i].sidewaysFriction;
+                    wfc.stiffness = Mathf.Clamp(sideWaysGrip - getSurface(hit.collider.gameObject.tag).gripLoss,0,1);
+                    wheels[i].sidewaysFriction = wfc;
+
+                    wfc = wheels[i].forwardFriction;
+                    wfc.stiffness = Mathf.Clamp(frontGrip - getSurface(hit.collider.gameObject.tag).gripLoss, 0, 1);
+                    wheels[i].forwardFriction = wfc;
+                }
+                else
+                {
+                    wfc = wheels[i].sidewaysFriction;
+                    wfc.stiffness = sideWaysGrip;
+                    wheels[i].sidewaysFriction = wfc;
+                    wfc = wheels[i].forwardFriction;
+                    wfc.stiffness = frontGrip;
+                    wheels[i].forwardFriction = wfc;
+                }
+            }
+        }
+    }
+
+    bool hasSurface(string s)
+    {
+        bool temp = false;
+
+        for (int i = 0; i < surfaces.Length; i++)
+        {
+            if (surfaces[i].name == s)
+                temp = true;
+        }
+
+        return temp;
+    }
+
+    Surface getSurface(string s)
+    {
+        for (int i = 0; i < surfaces.Length; i++)
+        {
+            if (surfaces[i].name == s)
+            {
+                return surfaces[i];
+            }
+        }
+
+        return new Surface();
+    }
+}
+[System.Serializable]
+public struct Surface
+{
+    public string name;
+    public float gripLoss;
+
+}
