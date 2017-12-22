115a543fb350da6ab333a7b9159f19a4690607ce
Boris Pettersson <br.cupofteagames@gmail.com>
2017-12-22 16:52:38 -0400

    Android Run

---
+++ Assets/Scripts/Game Scripts/MobileOrientation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileOrientation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        Screen.orientation = ScreenOrientation.Landscape;
+
+        Screen.autorotateToLandscapeLeft = true;
+
+        Screen.autorotateToLandscapeRight = true;
+
+        Screen.orientation = ScreenOrientation.AutoRotation;
+
+    }
+	
+}
