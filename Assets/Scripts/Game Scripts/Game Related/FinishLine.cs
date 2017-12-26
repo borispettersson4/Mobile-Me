c4b5722b3d6fa07f4bbddc64435bbffd2eaa4568
Boris Pettersson <br.cupofteagames@gmail.com>
2017-12-26 16:58:00 -0400

    Job 5

--- Assets/Scripts/Game Scripts/Game Related/FinishLine.cs
+++ Assets/Scripts/Game Scripts/Game Related/FinishLine.cs
@@ -6,11 +6,12 @@ public class FinishLine : MonoBehaviour {
 
 
     public GUIManager manager;
+    public Player player;
 
 
     void OnTriggerEnter(Collider col)
     {
-        if (col.gameObject.tag == "Car")
+        if (col.gameObject.tag == "Car" && !player.isDestroyed() & player.isStill())
         {
             manager.hideGUI();
             manager.playFinishGUI();
--- Assets/Scripts/Game Scripts/Token.cs
+++ Assets/Scripts/Game Scripts/Token.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Token : MonoBehaviour {
 
     public TokenCounter tokenCounter;
+    public AudioSource gainSound;
     bool active = true;
 
     void OnTriggerEnter(Collider col)
     {
     if(col.gameObject.tag == "Car" && active)
         {
+            gainSound.Play();
             active = false;
             Destroy(gameObject);
             tokenCounter.addTokens(1);
