1ad842341e4afb8c7aabb3b0c0ac190fd3e446be
DESKTOP-DA3D7HN\rende <renderbydavid@yahoo.com>
2017-12-27 17:16:16 -0600

    Jobs 2,4

---
+++ Assets/Scripts/Game Scripts/AnimationTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTrigger : MonoBehaviour {
+
+   
+    public GameObject Anim;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            
+            
+            Anim.GetComponent<Animator>().enabled = true; ;
+           
+            
+        }
+    }
+}
