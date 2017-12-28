df20ed0cbdeed4dcced6f9897d0e921bd0db3e5c
DESKTOP-DA3D7HN\rende <renderbydavid@yahoo.com>
2017-12-28 15:09:07 -0600

    Lvl8collision

---
+++ Assets/Scripts/Game Scripts/Experiremental/RigidbodyTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodyTrigger : MonoBehaviour {
+
+    public Rigidbody Collider;
+
+    public GameObject Chassis;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (Chassis.GetComponent<BoxCollider>().enabled == true)
+        {
+
+
+            Collider.isKinematic = false;
+
+
+        }
+    }
+}
