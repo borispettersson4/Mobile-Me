bb379e2f326b66d17aded3aa39b3f7d2f7c7d494
Boris Pettersson <br.cupofteagames@gmail.com>
2017-12-28 16:52:57 -0400

    Added Toll Script

---
+++ Assets/Scripts/Game Scripts/Toll.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toll : MonoBehaviour {
+
+    public TokenCounter tokenCounter;
+    public Animation animationClip;
+    public float tokenCount;
+
+    bool active1 = true;
+
+    void Update()
+    {
+        if(tokenCounter.getTokenCount() >= tokenCount && active1)
+        {
+            active1 = false;
+            GetComponent<Animator>().Play(animationClip.name);
+        }
+    }
+}
